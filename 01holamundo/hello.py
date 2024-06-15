7dabba578b8da7f74f56828a934d60cdd47f7298
Juan-Gines <JuangAlenta@hotmail.com>
2024-06-15 13:59:24 +0200

    Lección 1 hola mundo y tipos de datos

---
+++ 01holamundo/hello.py
@@ -0,0 +1,26 @@
+# Esto es un comentario de una sola línea
+# El hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+
+"""
+  Este es un comentario
+  de varias líneas
+"""
+
+'''
+  Este es un comentario
+  de varias líneas
+'''
+
+# Consultar el tipo de dato
+print(type("Hola Python")) # <class 'str'>
+print(type(5)) # <class 'int'>
+print(type(5.5)) # <class 'float'>
+print(type(5 + 5j)) # <class 'complex'>
+print(type(True)) # <class 'bool'>
+print(type(False)) # <class 'bool'>
+print(type([1, 2, 3])) # <class 'list'>
+print(type((1, 2, 3))) # <class 'tuple'>
+print(type({1, 2, 3})) # <class 'set'>
+print(type({1: "uno", 2: "dos", 3: "tres"})) # <class 'dict'>
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Curso de python
+
+Explorando y aprendiendo python con Mouredev.
+
+## Agradecimientos
+
+Gracias a Mouredev por compartir sus conocimientos y guiarme en este fantastico mundo de python.
